2d56a64d88afd07916320a84424d3bf282aba141
frenchbaguetteman <tigerwu200848@gmail.com>
2024-04-08 12:50:13 -0600

    changed stuff
    stuff

--- transforms.py
+++ transforms.py
@@ -1,10 +1,8 @@
 import numpy as np
 
-def transform(fileName):
-    with open(fileName, 'r') as file:
-        array = np.loadtxt(file, delimiter=',', dtype=float)
-    transformedArray = np.fft.rfft(array)
-    return transformedArray
+def transform(tensor):
+
+    return np.fft.rfft(tensor.eval())
 
 def writeArray(array, fileName):
     np.savetxt(fileName, array, delimiter=',', fmt='%0.8f')
